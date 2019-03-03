1f0389e5414dd7ae12b3d36a45b756bfdc0d3db7
DasneiN <sank.mail@gmail.com>
2019-03-04 00:41:02 +0300

    feat: implement currency change

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
 import Table from './components/table.js';
 
-import users from '../data/users.json';
-import companies from '../data/companies.json';
-import orders from '../data/orders.json';
-
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -25,14 +21,6 @@ export default (function () {
   const container = document.createElement('div');
   container.classList.add('container-fluid');
   app.appendChild(container);
-
-  // fetch('https://api.exchangeratesapi.io/latest?base=USD')
-  //   .then((res, err) => {
-  //     return res.json();
-  //   })
-  //   .then((r, e) => {
-  //     console.log(r);
-  //   });
 
   Promise.all([
     fetch('http://localhost:9000/api/companies.json').then(v => v.json()),
